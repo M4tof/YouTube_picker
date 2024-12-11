801a1129e96cfd324966a88a703205a3be52c297
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2024-12-11 17:03:15 +0100

    Gui interface start, file loading

---
+++ Facts.clp
@@ -0,0 +1,2 @@
+(deffacts startup
+   (start))
--- Main.py
+++ Main.py
@@ -1,2 +1,31 @@
-import tkinter
+import tkinter as tk
+import clips
 
+environment = clips.Environment()
+environment.load('Rules.clp')
+environment.load('Facts.clp')
+
+environment.reset()
+print("Rules and Facts loaded:")
+
+j = 0
+for rule in environment.rules():
+    print(f"{j}: {rule}")
+    j = j + 1
+    
+i = 0
+for fact in environment.facts():
+    print(f"{i}: {fact}")
+    i = i + 1
+
+
+print("\n---\nMain loop start: terminal for debug\n---\n")
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.title("So you want to watch YouTube")
+    root.config(bg="#262626")
+    root.geometry("650x500")
+
+    root.resizable(False, False)
+    root.mainloop()
